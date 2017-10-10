c9e7e82ea7eb36ba266dd2061a9b12c3f23c4d16
ssmak <steave.mkm@hotmail.com>
2017-10-10 13:02:00 +0800

    Add demo page.

--- dev/cc-compile.bat
+++
@@ -1,7 +0,0 @@
-::
-::The JS source using the Google Closure Compiler to optimize.
-::
-@echo off
-::Point to your self location, the worda are self explain.
-java -jar c:/cc/compiler.jar --js ./../jquery-tcsc-convert.js --js_output_file ./../jquery-tcsc-convert.min.js
-pause
